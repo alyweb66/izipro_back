7ba92adcf50f7db21ade1178d004c5003bd62dbc
alyweb66 <alyweb66@gmail.com>
2024-02-13 12:10:48 +0100

    add user schema and resolver is working

---
+++ app/datasources/data/datamappers/User.js
@@ -0,0 +1,7 @@
+import CoreDatamapper from './CoreDatamapper.js';
+
+class User extends CoreDatamapper {
+  tableName = 'user';
+}
+
+export default User;
--- app/resolvers/index.js
+++ app/resolvers/index.js
@@ -1,6 +1,9 @@
 import Query from './query.js';
+import Message from './message.js';
+import User from './user.js';
 
 export default {
   Query,
-
+  Message,
+  User,
 };
