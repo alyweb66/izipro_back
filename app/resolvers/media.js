65d2de628c7d4fd726e463beaa27980ea10153e1
alyweb66 <alyweb66@gmail.com>
2024-02-15 09:28:50 +0100

    add media schema and add test

---
+++ app/resolvers/media.js
@@ -0,0 +1,9 @@
+import Debug from 'debug';
+
+const debug = Debug(`${process.env.DEBUG_MODULE}:resolver:media`);
+
+const MediaResolver = {
+
+};
+debug('MessageResolver');
+export default MediaResolver;
