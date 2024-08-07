0f6e6058193de9203c83debc6d3f279f858a4b4b
alyweb66 <alyweb66@gmail.com>
2024-08-07 10:51:18 +0200

    fix sqitch for resetDb

--- migrations/revert/init.sql
+++ migrations/revert/init.sql
@@ -3,7 +3,6 @@
 BEGIN;
 
 DROP TABLE "user_has_hiddingClientRequest";
-DROP TABLE "user_has_user";
 DROP TABLE "user_has_job";
 DROP TABLE "user_has_notViewedRequest";
 DROP TABLE "user_has_notViewedConversation";
@@ -11,6 +10,7 @@ DROP TABLE "message_has_media" CASCADE;
 DROP TABLE "request_has_media" CASCADE;
 DROP TABLE "message";
 DROP TABLE "notification_push";
+DROP TABLE "notification";
 DROP TABLE "media";
 DROP TABLE "conversation";
 DROP TABLE "request";
@@ -25,10 +25,10 @@ DROP TABLE "user";
 DROP DOMAIN email;
 DROP DOMAIN postal_code_domain;
 
-DROP INDEX [IF EXISTS] "idx_user_email";
-DROP INDEX [IF EXISTS] "idx_request_user_id";
-DROP INDEX [IF EXISTS] "idx_message_conversation_id";
-DROP INDEX [IF EXISTS] "idx_conversation_request_id";
+DROP INDEX IF EXISTS "idx_user_email";
+DROP INDEX IF EXISTS "idx_request_user_id";
+DROP INDEX IF EXISTS "idx_message_conversation_id";
+DROP INDEX IF EXISTS "idx_conversation_request_id";
 
 
 COMMIT;
