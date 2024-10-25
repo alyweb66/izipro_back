37a12324d66e3074ca93b26bc83160f15684cd55
alyweb66 <alyweb66@gmail.com>
2024-10-25 16:07:09 +0200

    refacto hide request

--- app/resolvers/userHiddenClientRequestMutation.js
+++ app/resolvers/userHiddenClientRequestMutation.js
@@ -14,7 +14,7 @@ function debugInDevelopment(message = '', value = '') {
  * @async
  * @function createHiddenClientRequest
  * @param {Object} _ - The parent object, which is not used in this resolver.
- * @param {{user_id: number, request_id: number}} input -
+ * @param {{user_id: number, request_id: number, isWithConversation?: Boolean}} input -
  * The input object containing user and request details.
  * @param {Object} context - The context object,
  * which contains dataSources and other contextual information.
@@ -29,16 +29,52 @@ async function createHiddenClientRequest(_, { input }, { dataSources }) {
 
   try {
     if (dataSources.userData.id !== input.user_id) {
-      throw new GraphQLError('Unauthorized', { extensions: { code: 'UNAUTHORIZED' , httpStatus: 401} });
+      throw new GraphQLError('Unauthorized', { extensions: { code: 'UNAUTHORIZED', httpStatus: 401 } });
     }
+
+    const { isWithConversation, ...newInput } = input;
+
     // create hidden client request
     const isCreatedHiddenClientRequest = await
     dataSources.dataDB.userHasHiddingClientRequest.create(
-      input,
+      newInput,
     );
     if (!isCreatedHiddenClientRequest) {
       throw new GraphQLError('Error creating hidden client request', { extensions: { code: 'BAD_REQUEST', httpStatus: 400 } });
     }
+
+    if (!input.isWithConversation || input.isWithConversation === false) {
+      return isCreatedHiddenClientRequest;
+    }
+
+    // remove subcription for this client conversation
+    const subscription = await dataSources.dataDB.subscription.findByUser(input.user_id);
+
+    const userConversations = await
+    dataSources.dataDB.conversation.getConversationByUser(input.user_id);
+
+    const conversationToDelete = userConversations
+      .find((conversation) => conversation.request_id === input.request_id).id;
+
+    // remove request_id in the array of subscriber_id
+    // for subscriber request and subscriber conversation
+    const subscriberRequestIds = subscription
+      ?.filter((sub) => sub.subscriber === 'clientConversation')
+      .flatMap((sub) => sub.subscriber_id) || [];
+
+    // remove the conversation id from the subscriberRequestIds
+    const updatedSubscriberConversationIds = subscriberRequestIds
+      .filter((id) => id !== conversationToDelete);
+
+    // Update subscription for request and conversation and delete not viewed conversation
+    await Promise.all([
+      dataSources.dataDB.subscription.createSubscription(input.user_id, 'clientConversation', updatedSubscriberConversationIds),
+      dataSources.dataDB.userHasNotViewedConversation.deleteNotViewedConversation({
+        user_id: input.user_id,
+        conversation_id: [conversationToDelete], // Convert Set back to Array
+      }),
+    ]);
+    // create new
 
     return isCreatedHiddenClientRequest;
   } catch (error) {
